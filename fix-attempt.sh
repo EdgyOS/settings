f5320ed314fafe400508392c208421550d8bd7b4
Daniella Mesquita <mesquitadaniella@proton.me>
2023-12-06 17:03:32 -0300

    Updates, add firstlogon_tour scripts

---
+++ fix-attempt.sh
@@ -0,0 +1,8 @@
+flutter packages upgrade
+#flutter pub upgrade --major-versions
+flutter clean
+#flutter pub cache repair #https://stackoverflow.com/a/70152299
+flutter pub get
+flutter create .
+#from https://stackoverflow.com/a/71549404
+flutter run
---
+++ init-4.sh
@@ -0,0 +1,17 @@
+#sudo apt -y install git curl cmake meson make clang libgtk-3-dev pkg-config && mkdir -p ~/development && cd ~/development && git clone https://github.com/flutter/flutter.git -b stable && echo 'export PATH="$PATH:$HOME/development/flutter/bin"' >> ~/.bashrc && source ~/.bashrc
+#cd ..
+#flutter pub get
+flutter run
+
+#flutter pub get
+#flutter create --platforms=linux .
+#dart pub global activate flutter_to_debian
+#export PATH="$PATH":"$HOME/.pub-cache/bin"
+##mkdir include
+
+##remainings of firstlogon_tour:
+##git clone https://github.com/Floflis/linux-icon-getter.git include/linux-icon-getter
+##chmod +x include/linux-icon-getter/linux-icon-getter
+
+##git clone https://github.com/Floflis/firstlogon-tour_cli.git include/firstlogon-tour_cli
+##chmod +x include/firstlogon-tour_cli/core.sh
